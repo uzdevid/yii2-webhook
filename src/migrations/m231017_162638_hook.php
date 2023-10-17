e4483df4d9e49456a6c272035545db22acc8a7e5
uzdevid <uzdevid@gmail.com>
2023-10-17 21:48:02 +0500

    Добавил миграции

---
+++ src/migrations/m231017_162638_hook.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231017_162638_hook
+ */
+class m231017_162638_hook extends Migration {
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void {
+        $this->createTable('hook', [
+            'id' => $this->primaryKey(),
+            'url' => $this->string(255)->notNull(),
+            'events' => $this->json()->notNull(),
+            'auth' => $this->json()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): bool {
+        $this->dropTable('hook');
+        return true;
+    }
+}
---
+++ src/migrations/m231017_162723_attempt.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231017_162723_attempt
+ */
+class m231017_162723_attempt extends Migration {
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void {
+        $this->createTable('attempt', [
+            'id' => $this->primaryKey(),
+            'hook_id' => $this->integer()->notNull(),
+            'event' => $this->string(255)->notNull(),
+            'payload' => $this->json()->notNull(),
+            'response' => $this->text()->notNull(),
+            'status' => $this->string(255)->notNull(),
+            'create_time' => $this->dateTime()->notNull(),
+        ]);
+
+        $this->addForeignKey('fk_attempt_hook_id', 'attempt', 'hook_id', 'hook', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): bool {
+        $this->dropTable('attempt');
+        return true;
+    }
+}
