f6fe1adca02a9ce0a311d02c88ae1020b982e7c0
uzdevid <uzdevid@gmail.com>
2023-11-15 13:25:57 +0500

    Добавил Url

--- src/module/views/hook/index.php
+++ src/module/views/hook/index.php
@@ -18,17 +18,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <header class="mb-5">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/webhook">WebHook</a>
+            <a class="navbar-brand" href="<?php echo Url::toRoute(['/webhook']); ?>">WebHook</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="/webhook/attempt/index">Attempts</a>
+                        <a class="nav-link" href="<?php echo Url::toRoute(['/webhook/attempt/index']); ?>">Attempts</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="/webhook/hook/index">Hooks</a>
+                        <a class="nav-link active" href="<?php echo Url::toRoute(['/webhook/hook/index']); ?>">Hooks</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/webhook/event/index">Events</a>
+                        <a class="nav-link" href="<?php echo Url::toRoute(['/webhook/event/index']); ?>">Events</a>
                     </li>
                 </ul>
             </div>
